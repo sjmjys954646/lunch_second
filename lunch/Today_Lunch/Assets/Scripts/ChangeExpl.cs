507bf20b845c59ccb3918831b63cb08b5d810552
sjmjys954646 <182605@jnu.ac.kr>
2018-09-05 23:49:40 +0900

    2018.09.05 yoon b
    도감

---
+++ lunch/Today_Lunch/Assets/Scripts/ChangeExpl.cs
@@ -0,0 +1,187 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+public class ChangeExpl : MonoBehaviour {
+
+    public GameObject Dogammanager;
+    public Dogam Dogams;
+    public int num;
+
+    private void Start()
+    {
+        Dogams = Dogammanager.GetComponent<Dogam>();
+    }
+
+    public void ChangeExplanation()
+    {
+        if (Dogams.page == 1)
+        {
+            for (int i = 0; i <= 9; i++)
+            {
+                if (gameObject.GetComponent<ChangeExpl>().num == i)
+                {
+                    Dogams.explan.text = "이름: " + Dogams.Data.Shop[i].Storename + "\n" 
+                        + "음식종류: " + Dogams.Data.Shop[i].Foodkind + "\n" 
+                        + "장소: " + Dogams.Data.Shop[i].Pos + "\n"
+                        + "가격대: " + Dogams.Data.Shop[i].Price + "\n" 
+                        + "설명: " + Dogams.Data.Shop[i].Explanation;
+                }
+            }
+        }
+        if (Dogams.page == 2)
+        {
+            for (int i = 0; i <= 9; i++)
+            {
+                if (gameObject.GetComponent<ChangeExpl>().num == i)
+                {
+                    Dogams.explan.text = "이름: " + Dogams.Data.Shop[i+10].Storename + "\n"
+                        + "음식종류: " + Dogams.Data.Shop[i+10].Foodkind + "\n"
+                        + "장소: " + Dogams.Data.Shop[i+10].Pos + "\n"
+                        + "가격대: " + Dogams.Data.Shop[i+10].Price + "\n"
+                        + "설명: " + Dogams.Data.Shop[i+10].Explanation;
+                }
+            }
+        }
+        if (Dogams.page == 3)
+        {
+            for (int i = 0; i <= 9; i++)
+            {
+                if (gameObject.GetComponent<ChangeExpl>().num == i)
+                {
+                    Dogams.explan.text = "이름: " + Dogams.Data.Shop[i+20].Storename + "\n"
+                        + "음식종류: " + Dogams.Data.Shop[i+20].Foodkind + "\n"
+                        + "장소: " + Dogams.Data.Shop[i+20].Pos + "\n"
+                        + "가격대: " + Dogams.Data.Shop[i+20].Price + "\n"
+                        + "설명: " + Dogams.Data.Shop[i+20].Explanation;
+                }
+            }
+        }
+        if (Dogams.page == 4)
+        {
+            for (int i = 0; i <= 9; i++)
+            {
+                if (gameObject.GetComponent<ChangeExpl>().num == i)
+                {
+                    Dogams.explan.text = "이름: " + Dogams.Data.Shop[i+30].Storename + "\n"
+                        + "음식종류: " + Dogams.Data.Shop[i+30].Foodkind + "\n"
+                        + "장소: " + Dogams.Data.Shop[i+30].Pos + "\n"
+                        + "가격대: " + Dogams.Data.Shop[i+30].Price + "\n"
+                        + "설명: " + Dogams.Data.Shop[i+30].Explanation;
+                }
+            }
+        }
+        if (Dogams.page == 5)
+        {
+            for (int i = 0; i <= 9; i++)
+            {
+                if (gameObject.GetComponent<ChangeExpl>().num == i)
+                {
+                    Dogams.explan.text = "이름: " + Dogams.Data.Shop[i+40].Storename + "\n"
+                        + "음식종류: " + Dogams.Data.Shop[i+40].Foodkind + "\n"
+                        + "장소: " + Dogams.Data.Shop[i+40].Pos + "\n"
+                        + "가격대: " + Dogams.Data.Shop[i+40].Price + "\n"
+                        + "설명: " + Dogams.Data.Shop[i+40].Explanation;
+                }
+            }
+        }
+        if (Dogams.page == 6)
+        {
+            for (int i = 0; i <= 9; i++)
+            {
+                if (gameObject.GetComponent<ChangeExpl>().num == i)
+                {
+                    Dogams.explan.text = "이름: " + Dogams.Data.Shop[i+50].Storename + "\n"
+                        + "음식종류: " + Dogams.Data.Shop[i+50].Foodkind + "\n"
+                        + "장소: " + Dogams.Data.Shop[i+50].Pos + "\n"
+                        + "가격대: " + Dogams.Data.Shop[i+50].Price + "\n"
+                        + "설명: " + Dogams.Data.Shop[i+50].Explanation;
+                }
+            }
+        }
+        if (Dogams.page == 7)
+        {
+            for (int i = 0; i <= 9; i++)
+            {
+                if (gameObject.GetComponent<ChangeExpl>().num == i)
+                {
+                    Dogams.explan.text = "이름: " + Dogams.Data.Shop[i+60].Storename + "\n"
+                        + "음식종류: " + Dogams.Data.Shop[i+60].Foodkind + "\n"
+                        + "장소: " + Dogams.Data.Shop[i+60].Pos + "\n"
+                        + "가격대: " + Dogams.Data.Shop[i+60].Price + "\n"
+                        + "설명: " + Dogams.Data.Shop[i+60].Explanation;
+                }
+            }
+        }
+        if (Dogams.page == 8)
+        {
+            for (int i = 0; i <= 9; i++)
+            {
+                if (gameObject.GetComponent<ChangeExpl>().num == i)
+                {
+                    Dogams.explan.text = "이름: " + Dogams.Data.Shop[i+70].Storename + "\n"
+                        + "음식종류: " + Dogams.Data.Shop[i+70].Foodkind + "\n"
+                        + "장소: " + Dogams.Data.Shop[i+70].Pos + "\n"
+                        + "가격대: " + Dogams.Data.Shop[i+70].Price + "\n"
+                        + "설명: " + Dogams.Data.Shop[i+70].Explanation;
+                }
+            }
+        }
+        if (Dogams.page == 9)
+        {
+            for (int i = 0; i <= 9; i++)
+            {
+                if (gameObject.GetComponent<ChangeExpl>().num == i)
+                {
+                    Dogams.explan.text = "이름: " + Dogams.Data.Shop[i+80].Storename + "\n"
+                        + "음식종류: " + Dogams.Data.Shop[i+80].Foodkind + "\n"
+                        + "장소: " + Dogams.Data.Shop[i+80].Pos + "\n"
+                        + "가격대: " + Dogams.Data.Shop[i+80].Price + "\n"
+                        + "설명: " + Dogams.Data.Shop[i+80].Explanation;
+                }
+            }
+        }
+        if (Dogams.page == 10)
+        {
+            for (int i = 0; i <= 9; i++)
+            {
+                if (gameObject.GetComponent<ChangeExpl>().num == i)
+                {
+                    Dogams.explan.text = "이름: " + Dogams.Data.Shop[i+90].Storename + "\n"
+                        + "음식종류: " + Dogams.Data.Shop[i+90].Foodkind + "\n"
+                        + "장소: " + Dogams.Data.Shop[i+90].Pos + "\n"
+                        + "가격대: " + Dogams.Data.Shop[i+90].Price + "\n"
+                        + "설명: " + Dogams.Data.Shop[i+90].Explanation;
+                }
+            }
+        }
+        if (Dogams.page == 11)
+        {
+            for (int i = 0; i <= 9; i++)
+            {
+                if (gameObject.GetComponent<ChangeExpl>().num == i)
+                {
+                    Dogams.explan.text = "이름: " + Dogams.Data.Shop[i+100].Storename + "\n"
+                        + "음식종류: " + Dogams.Data.Shop[i+100].Foodkind + "\n"
+                        + "장소: " + Dogams.Data.Shop[i+100].Pos + "\n"
+                        + "가격대: " + Dogams.Data.Shop[i+100].Price + "\n"
+                        + "설명: " + Dogams.Data.Shop[i+100].Explanation;
+                }
+            }
+        }
+        if (Dogams.page == 12)
+        {
+            for (int i = 0; i <= 3; i++)
+            {
+                if (gameObject.GetComponent<ChangeExpl>().num == i)
+                {
+                    Dogams.explan.text = "이름: " + Dogams.Data.Shop[i+110].Storename + "\n"
+                        + "음식종류: " + Dogams.Data.Shop[i+110].Foodkind + "\n"
+                        + "장소: " + Dogams.Data.Shop[i+110].Pos + "\n"
+                        + "가격대: " + Dogams.Data.Shop[i+110].Price + "\n"
+                        + "설명: " + Dogams.Data.Shop[i+110].Explanation;
+                }
+            }
+        }
+    }
+}
--- lunch/Today_Lunch/Assets/Scripts/Dogam.cs
+++ lunch/Today_Lunch/Assets/Scripts/Dogam.cs
@@ -7,10 +7,15 @@ public class Dogam : MonoBehaviour {
 
     public GameObject Dataget;
     public Database Data;
+    public GameObject ButtonsOb;
 
     public int page=1;
 
     public List<Text> texts = new List<Text>();
+    public List<Button> buttons = new List<Button>();
+
+    public Text Pagenum;
+    public Text explan;
 
     private void Start()
     {
@@ -23,7 +28,11 @@ public class Dogam : MonoBehaviour {
         if(page==1)
         {
             for(int i = 0;i<=9;i++)
-            {        
+            {
+                if(i==9)
+                {
+                    texts[i].fontSize = 40;
+                }
                 texts[i].text = Data.Shop[i].Storename;
             }
         }
@@ -41,5 +50,103 @@ public class Dogam : MonoBehaviour {
                 texts[i - 20].text = Data.Shop[i].Storename;
             }
         }
+        if (page == 4)
+        {
+            for (int i = 30; i <= 39; i++)
+            {
+                texts[i - 30].text = Data.Shop[i].Storename;
+            }
+        }
+        if (page == 5)
+        {
+            for (int i = 40; i <= 49; i++)
+            {
+                if (i == 41 || i == 43 || i == 46 || i==47)
+                {
+                    texts[i-40].fontSize = 40;
+                }
+                texts[i - 40].text = Data.Shop[i].Storename;
+            }
+        }
+        if (page == 6)
+        {
+            for (int i = 50; i <= 59; i++)
+            {
+                if (i == 53)
+                {
+                    texts[i - 50].fontSize = 40;
+                }
+                texts[i - 50].text = Data.Shop[i].Storename;
+            }
+        }
+        if (page == 7)
+        {
+            for (int i = 60; i <= 69; i++)
+            {
+                if (i == 62||i==63||i==67)
+                {
+                    texts[i - 60].fontSize = 40;
+                }
+                texts[i - 60].text = Data.Shop[i].Storename;
+            }
+        }
+        if (page == 8)
+        {
+            for (int i = 70; i <= 79; i++)
+            {
+                texts[i - 70].text = Data.Shop[i].Storename;
+            }
+        }
+        if (page == 9)
+        {
+            for (int i = 80; i <= 89; i++)
+            {
+                texts[i - 80].text = Data.Shop[i].Storename;
+            }
+        }
+        if (page == 10)
+        {
+            for (int i = 90; i <= 99; i++)
+            {
+                texts[i - 90].text = Data.Shop[i].Storename;
+            }
+        }
+        if (page == 11)
+        {
+            for (int i = 100; i <= 109; i++)
+            {
+                texts[i - 100].text = Data.Shop[i].Storename;
+            }
+        }
+        if (page == 12)
+        {
+            for (int i = 110; i <= 119; i++)
+            {
+                if(i>=114)
+                {
+                    texts[i - 110].text = " ";
+                    continue;          
+                }
+                texts[i - 110].text = Data.Shop[i].Storename;
+            }
+        }
+        if (page < 1)
+            page = 1;
+        if (page > 12)
+            page = 12;
+        Pagenum.text = "("+page+")";
     }
+
+    public void Button_Right()
+    {
+        page++;
+        Changetext();
+    }
+
+    public void Button_Left()
+    {
+        page--;
+        Changetext();
+    } 
+
 }
